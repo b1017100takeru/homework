1e3b47e653079a774c33612bc25bd94bbf2a10bd
Takeru Fujii <b1017100@fun.ac.jp>
2019-09-28 18:45:06 +0900

    API以外やったよ

---
+++ Homework/Homework/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  Homework
+//
+//  Created by 藤井偉 on 2019/09/28.
+//  Copyright © 2019 MIRAIMOBILE. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
