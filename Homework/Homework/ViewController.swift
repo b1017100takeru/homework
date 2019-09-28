7a0fe915c1ff99c2a4831228923e03b46325087b
Takeru Fujii <b1017100@fun.ac.jp>
2019-09-28 17:24:32 +0900

    作ったよ

---
+++ Homework/Homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by 藤井偉 on 2019/09/28.
+//  Copyright © 2019 MIRAIMOBILE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
